b07181bdac8529d924094876ab19c6a5117c55c1
Stefan <stefan.armijo@gmail.com>
2018-05-18 11:15:33 -0700

    created userController

--- server.js
+++ server.js
@@ -3,17 +3,40 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const pg = require('pg');
 
-const URI = process.env.DBURI;
+require('dotenv').config();
 const app = express();
 
-pg.connect(URI, (err, db) => {
-  if (err) {
-    throw new Error(err);
-  } else {
-    console.log('connected to db')
-  }
+// const pool = new Pool({
+//   connectionString: connectionString,
+// })
 
+// pool.query('SELECT * FROM films', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// });
+
+const client = new pg.Client({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  host: process.env.PGHOST,
+  ssl: true
 });
+
+client.connect()
+
+client.query('SELECT * FROM films', (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res.rows);
+  }
+  client.end()
+});
+
+// ROUTES
+
 
 const PORT = 5535;
 
@@ -21,7 +44,6 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
-module.exports = server;
 
 
 
