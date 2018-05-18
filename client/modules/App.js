4e61daefd86ec2ab4dce63c9be1dab511c7b376a
Christopher <chris132128@gmail.com>
2018-05-18 11:12:19 -0700

    redux reducer, actions, module skeleton

--- client/modules/App.js
+++ client/modules/App.js
@@ -1,5 +1,29 @@
-import Immutable from 'immutable';
+import Immutable from "immutable";
+
+export const ACTIONO = "ACTIONO";
 
 const defaultState = Immutable.fromJS({
-    key1: 'value',
-  });
+  key1: "value"
+});
+
+export function myFirstAction(bool) {
+  return (dispatch, getState) => {
+    return dispatch({
+      type: ACTIONO,
+      payload: bool
+    });
+  };
+}
+
+export const actions = {
+    myFirstAction
+  };
+
+const ACTION_HANDLERS = {
+  [ACTIONO]: (state, action) => state.set("key1", action.payload),
+};
+
+export default function appReducer(state = defaultState, action) {
+  const handler = ACTION_HANDLERS[action.type];
+  return handler ? handler(state, action) : state;
+}
