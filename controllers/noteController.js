924c96f1f85b170ac4c0427e97990da2dbf1a6d3
Stefan <stefan.armijo@gmail.com>
2018-05-18 15:24:10 -0700

    build note controller routes

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -11,55 +11,35 @@ const client = new pg.Client({
   ssl: true
 });
 
-client.connect(function(err) {
-  if(err) console.log("client connect: ", err);
-})
+client.connect(function (err) {
+  if (err) {
+    console.log("client connect: ", err);
+  }
+});
 
 
 const noteController = {
 
-  postdata : (req, res) => {
-    console.log(req.body);
+  getNotesByUser: {
 
-    
-      let {code, title, did, date_prod, kind, len} = req.body;
-      // let promise = new Promise((resolve, reject) => {
-        let q = `INSERT INTO films(code, title, "did", date_prod, kind, len) VALUES ('${code}', '${title}', '${did}', '${date_prod}', '${kind}', '${len}' );`;
-        console.log("Our query is read: ", q);
+  },
 
-        client.query(q, (err, results) => {
-          if (err) console.log(err)
-          else console.log(results);
-          // resolve();
-        });
-      // });
-  // Promise.all(promise)
-  // .then(() => {
-  //   console.log("Promise got resolved");
-  //   db.end()
-  // })
-  // .catch((err) => {
-  //   console.log("Error promise", err)});
-},
+  getNoteByID: {
 
-  getdata : (req, res) => {
+  },
 
-  client.query('SELECT * FROM ', (err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(res.rows);
+  createNote: {
+
+  },
+
+  deleteNote: {
+
+  },
+
+  updateNote: {
+
   }
-  client.end()
-});
-  }
+
 }
 
 module.exports = noteController; 
-
-
-
-// INSERT INTO films VALUES
-//     ('UA502', 'Bananas', 105, '1971-07-13', 'Comedy', '82 minutes');
-//        code  |  title  | did  | date_prod  |  kind  |   len  
-
