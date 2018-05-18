52a916627ad97bb03dccd626b7fa12217b174e5e
Christopher <chris132128@gmail.com>
2018-05-17 23:30:03 -0700

    deleted extra index, implemented react-router

--- client/Main.jsx
+++ client/Main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import Login from "./containers/Login.jsx";
+
 const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Login} />
+      <Route path="/home" component={Home} />
     </Switch>
   </main>
 );
