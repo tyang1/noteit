4d7882e27e98ae2bf47a14d348c7cbbb5892ac45
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-07-02 14:35:24 -0700

    review noteit

--- client/components/highlight.js
+++ client/components/highlight.js
@@ -1,6 +1,17 @@
-var range = window.getSelection().getRangeAt(0);
-var selectionContents = range.extractContents();
-var div = document.createElement("div");
-div.style.color = "yellow";
-div.appendChild(selectionContents);
-range.insertNode(div);
+// var range = window.getSelection().getRangeAt(0);
+// var selectionContents = range.extractContents();
+// var div = document.createElement("div");
+// div.style.color = "yellow";
+// div.appendChild(selectionContents);
+// range.insertNode(div);
+const fetch = require('node-fetch')
+
+fetch('https://en.wikipedia.org/wiki/Main_Page', {
+	method: 'get'
+}).then(function(response) {
+	return response.text()
+}).then((html) => {
+    console.log(`html is ${html}`)
+}).catch(function(err) {
+	console.log(err);
+});
